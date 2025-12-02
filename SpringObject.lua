d1cb16cc0debc3e44996035a434554a5b5550ef0
saydnyc <saydnyc@gmail.com>
2025-12-02 03:32:07 -0500

    Refactor SpringObject properties and calculations

--- SpringObject.lua
+++ SpringObject.lua
@@ -1,8 +1,4 @@
 local SpringObject = {
-	MaxSpringLength    = 10,
-	TargetSpringLength = 3,
-	WheelRadius        = 1.9,
-
 	SpringLength       = 0,
 	LastSpringLength   = 0,
 
@@ -10,8 +6,10 @@ local SpringObject = {
 	SpringDisplacement = 0,
 	SpringVelocity     = 0,
 
+	Compression = 0,
 	StiffnessForce     = 0,
 	DamperForce        = 0,
+	ForceVector        = Vector3.new(),
 
 	Raycast            = nil,
 	RaycastPosition    = Vector3.new(),
@@ -19,17 +17,22 @@ local SpringObject = {
 	RaycastDistance    = 0,
 
 	CFrame             = CFrame.new(),
-	Attachment         = Instance.new("Attachment"),
-	RaycastParams      = nil,
+	Attachment         = nil,
+	RaycastParams      = RaycastParams.new(),
+	
 
-	Damper             = 3000,
-	Stiffness          = 100000,
+	MaxSpringLength    = 1.5,
+	TargetSpringLength = 1.3,
+	WheelRadius        = 2.669/2,
+	Damper             = 1000,
+	Stiffness          = 30000,
+	UseGlobalUpVector = true
 }
-
 
 SpringObject.__index = SpringObject
 
 function SpringObject:Frame(DeltaTime)
+	DeltaTime = DeltaTime or 1
 	if not self.Attachment or not self.RaycastParams then return end
 
 	self.RaycastPosition = self.Attachment.WorldCFrame.Position
@@ -45,26 +48,27 @@ function SpringObject:Frame(DeltaTime)
 	self.SpringLength = if self.Raycast then self.Raycast.Distance - self.WheelRadius else self.MaxSpringLength
 
 	self.SpringDisplacement = self.TargetSpringLength - self.SpringLength
+
 	self.SpringVelocity = (self.SpringLength - self.LastSpringLength) / DeltaTime
 	self.LastSpringLength = self.SpringLength
 
 	self.StiffnessForce = self.Stiffness * self.SpringDisplacement
 	self.DamperForce    = self.Damper * -self.SpringVelocity
 
+	local Force = self.StiffnessForce + self.DamperForce
+
 	self.CFrame = self.Attachment.WorldCFrame:ToWorldSpace(CFrame.new(0, -self.SpringLength, 0))
 
-	self.SpringForce = if self.Raycast then self.StiffnessForce + self.DamperForce else 0
+	self.Compression = math.clamp(1 - (self.SpringLength / self.TargetSpringLength), 0,1)
+	self.SpringForce = if self.Raycast then Force else 0
+	self.ForceVector = if self.UseGlobalUpVector then self.SpringForce * Vector3.yAxis else self.SpringForce * self.Attachment.CFrame.UpVector
 end
-
 
 function SpringObject.New(attachment)
 	local self = setmetatable({}, SpringObject)
-	self.RaycastParams = RaycastParams.new()
-	self.RaycastParams.FilterType = Enum.RaycastFilterType.Exclude
 	self.Attachment = attachment
-	
+	self.RaycastParams.FilterType = Enum.RaycastFilterType.Exclude
 	return self
 end
-
 
 return SpringObject
